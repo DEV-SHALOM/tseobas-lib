3436d30a4f8293a743053858e54f109eb61e9fde
DEV-SHALOM <dev.shalom1@gmail.com>
2025-06-30 12:52:30 +0100

    testing  the new update of the passowrd eye checker

--- src/script.js
+++ src/script.js
@@ -1,21 +1,16 @@
-let password = document.getElementById("password");
-let icon = document.getElementById("icon");
-let passButton = document.getElementById("pass-button");
-let checkbox = document.getElementById("checkbox");
-// let hidden = true;
+const password = document.getElementById("password");
+const icon = document.getElementById("icon");
 
-passButton.addEventListener("click", () => {
-  if (checkbox.checked === false) {
-    icon.classList.remove("fa-eye");
-    icon.classList.add("fa-eye-slash");
-    password.setAttribute("type", "text");
-    checkbox.checked = true;
-  } else if (checkbox.checked === true) {
+icon.addEventListener("click", () => {
+  const isPasswordVisible = password.getAttribute("type") === "text";
+
+  if (isPasswordVisible) {
+    password.setAttribute("type", "password");
     icon.classList.remove("fa-eye-slash");
     icon.classList.add("fa-eye");
-    password.setAttribute("type", "password");
-    checkbox.checked = false;
   } else {
-    // ~~~~~~~~
+    password.setAttribute("type", "text");
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
   }
 });
